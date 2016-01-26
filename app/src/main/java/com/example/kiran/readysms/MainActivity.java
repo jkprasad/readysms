7eb3eb7abe7e7ecbd3aefec98803eb14c5562de6
kiran juthi <jkprasad@gmail.com>
2016-01-26 22:39:34 +0530

    This is the changes list to start with for the project

---
+++ app/src/main/java/com/example/kiran/readysms/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.kiran.readysms;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        String[] items = {"Touch here to create a readily available SMS for a contact"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, items);
+
+        setListAdapter(adapter);
+
+    }
+}
